9757b5c6ef9fae97fd1d63d8790447b16b6a295b
Valihan <valihanburhanov@yandex.ru>
2023-02-06 16:36:07 +0300

    task3 complete

--- c#hw8.cs
+++ c#hw8.cs
@@ -7,64 +7,64 @@
 18 20
 15 18
 */
-
-
-int[,] table = new int[2, 2];
-
-// Функция заполнения массива рандомно числами от 1 до 9
-void FillFirstMatrixRandom(int[,] firstMatrix)
+int[,] firstMatrix = new int[2,2];
+int[,] secondMatrix = new int[2,2];
+int[,] resultMatrix = new int[2,2];
+int[] lineFirstMatrix = new int[4];
+int[] lineSecondMatrix = new int[4];
+int p = 0;
+for (int i = 0; i < firstMatrix.GetLength(0); i++)
 {
-    for (int i = 0; i < firstMatrix.GetLength(0); i++)
+    for (int j = 0; j < firstMatrix.GetLength(1); j++)
     {
-        for (int j = 0; j < firstMatrix.GetLength(1); j++)
-        {
-            firstMatrix[i, j] = new Random().Next(1, 10);
-        }
+        firstMatrix[i, j] = new Random().Next(0, 10);
+        secondMatrix[i,j] = new Random().Next(0,10);
     }
 }
-void PrintFirstMatrix(int[,] firstMatrix)
-{
-    for (int i = 0; i < firstMatrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < firstMatrix.GetLength(1); j++)
-        {
-            Console.Write($"{firstMatrix[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-void FillSecondMatrixRandom(int[,] secondMatrix)
-{
-    for (int i = 0; i < secondMatrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < secondMatrix.GetLength(1); j++)
-        {
-            secondMatrix[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-void PrintSecondMatrix(int[,] secondMatrix)
-{
-    for (int i = 0; i < secondMatrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < secondMatrix.GetLength(1); j++)
-        {
-            Console.Write($"{secondMatrix[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
-void FindResultOfTwoMatrix(int[,] firstMatrix, int[,] secondMatrix)
-{
-    Console.WriteLine($"{firstMatrix[0,0]}, {secondMatrix[1,0]}");
-}
-
-
-
 Console.WriteLine("Первая матрица: ");
-FillFirstMatrixRandom(table);
-PrintFirstMatrix(table);
+for (int i = 0; i < firstMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < firstMatrix.GetLength(1); j++)
+    {
+        lineFirstMatrix[p] = firstMatrix[i,j];
+        Console.Write($"{firstMatrix[i,j]} ");
+        p++;
+    }
+    Console.WriteLine();;
+}
+p=0;
 Console.WriteLine("Вторая матрица: ");
-FillSecondMatrixRandom(table);
-PrintSecondMatrix(table);
+for (int i = 0; i < secondMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondMatrix.GetLength(1); j++)
+    {
+        lineSecondMatrix[p] = secondMatrix[i,j];
+        Console.Write($"{secondMatrix[i,j]} ");
+        p++;
+    }
+    Console.WriteLine();;
+}
+Console.WriteLine("Результирующая матрица: ");
+var matrix3 = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+if (firstMatrix.GetLength(1) == secondMatrix.GetLength(0))
+{
+    for (int i = 0; i < matrix3.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3.GetLength(1); j++)
+        {
+            matrix3[i, j] = 0;
+            for (int n = 0; n < firstMatrix.GetLength(1); n++)
+            {
+                matrix3[i, j] += firstMatrix[i, n] * secondMatrix[n, j];
+            }
+        }
+    }
+}
+for (int i = 0; i < matrix3.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix3.GetLength(1); j++)
+    {
+        Console.Write($"{matrix3[i,j]} ");
+    }
+    Console.WriteLine();
+}
